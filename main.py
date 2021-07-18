33ee628672d81ff5e74a312dbaafdf5f2a3f7e56
mmpott <87590070+mmpott@users.noreply.github.com>
2021-07-18 11:08:08 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+authenticate = requests.get('https://random-words-api.vercel.app/word')
+randomwords = authenticate.json()
+
+def _choose_word():
+    for item in randomwords:
+        return item['word']
+
+
+
+def _hint(word):
+    '''Returns the definition of the random word'''
+    for item in randomwords:
+        return item['definition']
+
+
+
+
